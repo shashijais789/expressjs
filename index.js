7ca6ba0eafd530d4c9b939d51218adda2caef75f
Shashikant Jaiswal <shashikant.jaiswal@broadcom.com>
2018-09-30 23:09:14 +0530

    Enabled CORS for localhost:4200

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ const express = require('express');
 const mysql = require('mysql');
 const app = express();
 app.use(express.json());
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    // Pass to next layer of middleware
+    next();
+});
 const courses = [{id: 1, name: 'Physics'}, {id: 2, name: 'Maths'}];
 const port = process.env.port || 8090;
 
